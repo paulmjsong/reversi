0c9b4ca1ed79e625765426b1c3be1f917f65a45e
Bread-bang <bang960527@gmail.com>
2023-06-05 00:27:49 +0900

    first commit

--- reversi.c
+++ reversi.c
@@ -7,29 +7,31 @@
 #include "header.h"
 
 /* ------ REVERSI ------ */
-
+/*
 void play (int conn_fd) {
 	do {
     // stuff
 	} while (1) ;
-}
+}*/
 
 /* -------- MAIN -------- */
 
 int main (int argc, char * argv[]) {
   // determine client or server
-  if (argc == 2 && !strcmp(argv[1], "-server")) {
-    int conn_fd = listen_at_port(atoi(argv[1])) ;
+
+  int conn_fd ;
+  if (argc == 3 && !strcmp(argv[1], "-server")) {
+	conn_fd = listen_at_port(atoi(argv[2])) ;
   }
-  else if (argc == 3 && !strcpy(argv[1], "-client")) {
-  	int conn_fd = connect_ipaddr_port(argv[1], atoi(argv[2])) ;
+  else if (argc == 4 && !strcmp(argv[1], "-client")) {
+  	conn_fd = connect_ipaddr_port(argv[2], atoi(argv[3])) ;
   }
   else {
     fprintf(stderr, "Error: Invalid argument.\n");
     return 1;
   }
 	
-	play(conn_fd) ;
+//	play(conn_fd) ;
 	shutdown(conn_fd, SHUT_RDWR) ;
   
   return 0;
